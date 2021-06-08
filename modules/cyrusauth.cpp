6243bb782c341774fbc218d123125b6868c08375
MrLenin <909621+MrLenin@users.noreply.github.com>
2021-06-08 03:25:35 -0400

    Extend SASLAuth
    Adds support for loading a SASL impersonation
    aware instance of the SASL module on newly cloned
    users on a specified network.
    
    Adds support for WEBIRC authentication to the network
    specified for SASL impersonation.

--- modules/cyrusauth.cpp
+++ modules/cyrusauth.cpp
@@ -21,10 +21,13 @@
  * @brief SASL authentication module for znc.
  */
 
-#include <znc/znc.h>
-#include <znc/User.h>
-
 #include <sasl/sasl.h>
+#include <znc/IRCNetwork.h>
+#include <znc/MD5.h>
+#include <znc/User.h>
+#include <znc/znc.h>
+
+#include <sstream>
 
 class CSASLAuthMod : public CModule {
   public:
@@ -38,6 +41,9 @@ class CSASLAuthMod : public CModule {
         m_cbs[1].proc = nullptr;
         m_cbs[1].context = nullptr;
 
+        m_bWEBIRC = false;
+        m_bSaslImpersonation = false;
+
         AddHelpCommand();
         AddCommand("Show", "", t_d("Shows current settings"),
                    [=](const CString& sLine) { ShowCommand(sLine); });
@@ -45,6 +51,22 @@ class CSASLAuthMod : public CModule {
                    t_d("Create ZNC users upon first successful login, "
                        "optionally from a template"),
                    [=](const CString& sLine) { CreateUsersCommand(sLine); });
+        AddCommand("SetSASLNetwork", t_d("<network name>"),
+                   t_d("Set the network to configure the SASL module for."),
+                   [=](const CString& sLine) { SetSASLNetworkCommand(sLine); });
+        AddCommand("SetSASL",
+                   t_d("no|yes [<mechanism> [<username> <password>]]|reset"),
+                   t_d("Configure the mechanism used by the SASL module and any"
+                       " necessary authentication credentials"),
+                   [=](const CString& sLine) { SetSASLCommand(sLine); });
+        AddCommand("SetWEBIRC",
+                   t_d("no|yes [<gateway> <password> <hostname>]|reset"),
+                   t_d("Configure the gateway, password and the hostname "
+                       "suffix used for WEBIRC connections."),
+                   [=](const CString& sLine) { SetWEBIRCCommand(sLine); });
+        AddCommand("SetUserSalt", t_d("<salt>"),
+                   t_d("Configure the salt used when hashing usernames. "),
+                   [=](const CString& sLine) { SetUserSaltCommand(sLine); });
     }
 
     ~CSASLAuthMod() override { sasl_done(); }
@@ -65,6 +87,10 @@ class CSASLAuthMod : public CModule {
         for (it = vsArgs.begin(); it != vsArgs.end(); ++it) {
             if (it->Equals("saslauthd") || it->Equals("auxprop")) {
                 m_sMethod += *it + " ";
+            } else if (it->Equals("webirc")) {
+                m_bWEBIRC = true;
+            } else if (it->Equals("impersonation")) {
+                m_bSaslImpersonation = true;
             } else {
                 CUtils::PrintError(
                     t_f("Ignoring invalid SASL pwcheck method: {1}")(*it));
@@ -89,7 +115,7 @@ class CSASLAuthMod : public CModule {
     }
 
     EModRet OnLoginAttempt(std::shared_ptr<CAuthBase> Auth) override {
-        const CString& sUsername = Auth->GetUsername();
+        const CString& sUsername = Auth->GetUsername().AsLower();
         const CString& sPassword = Auth->GetPassword();
         CUser* pUser(CZNC::Get().FindUser(sUsername));
         sasl_conn_t* sasl_conn(nullptr);
@@ -133,11 +159,50 @@ class CSASLAuthMod : public CModule {
                         pUser = nullptr;
                     }
 
-                    if (pUser && !pUser->Clone(*pBaseUser, sErr)) {
+                    if (pUser && !pUser->Clone(*pBaseUser, sErr, true)) {
                         DEBUG("saslauth: Clone User [" << CloneUser()
                                                        << "] failed: " << sErr);
                         delete pUser;
                         pUser = nullptr;
+                    }
+
+                    CString sUsernameAsTyped = Auth->GetUsername();
+
+                    pUser->SetNick(sUsernameAsTyped);
+                    pUser->SetAltNick(sUsernameAsTyped + "_");
+                    pUser->SetIdent(sUsernameAsTyped);
+                    pUser->SetRealName(sUsernameAsTyped);
+
+                    if (m_bSaslImpersonation) {
+                        CIRCNetwork* pNetwork =
+                            pUser->FindNetwork(GetNV("network_name"));
+
+                        if (!pNetwork) {
+                            DEBUG("saslauth: Find network ["
+                                  << GetNV("network_name")
+                                  << "] failed: " << sErr);
+                            delete pUser;
+                            pUser = nullptr;
+                        }
+
+                        if (pNetwork->GetModules().LoadModule(
+                                "sasl", "", CModInfo::NetworkModule, pUser,
+                                pNetwork, sErr)) {
+                            CModule* pModule =
+                                pNetwork->GetModules().FindModule("sasl");
+                            pModule->SetNV("impersonation", "yes");
+                            pModule->SetNV("username", GetNV("sasl_username"));
+                            pModule->SetNV("password", GetNV("sasl_password"));
+                            pModule->SetNV("mechanisms",
+                                           GetNV("sasl_mechanism"));
+                            pModule->SetNV("require_auth", "yes");
+                        } else {
+                            DEBUG(
+                                "saslauth: Load network module [sasl] failed: "
+                                << sErr);
+                            delete pUser;
+                            pUser = nullptr;
+                        }
                     }
                 }
 
@@ -158,6 +223,33 @@ class CSASLAuthMod : public CModule {
             if (pUser) {
                 Auth->AcceptLogin(*pUser);
                 return HALT;
+            }
+        }
+
+        return CONTINUE;
+    }
+
+    EModRet OnIRCRegistration(CString& sPass, CString& sNick, CString& sIdent,
+                              CString& sRealname) override {
+        if (m_bWEBIRC) {
+            CUser* pUser = GetUser();
+            CIRCNetwork* pNetwork = GetNetwork();
+
+            if (pUser && !pNetwork->GetName().CaseCmp(GetNV("network_name"))) {
+                std::ostringstream sWebIRCMsg;
+                CString sUsername = pUser->GetUsername();
+                CMD5 md5sum(sUsername + GetNV("user_salt"));
+                uint8* hashBytes = downsample(md5sum.GetHash());
+                uint hashInts[3] = {hashBytes[0], hashBytes[1], hashBytes[2]};
+
+                sWebIRCMsg << "WEBIRC " << GetNV("webirc_password") << " "
+                           << GetNV("webirc_gateway") << " " << sUsername << "."
+                           << GetNV("webirc_host_suffix") << " 255."
+                           << hashInts[0] << "." << hashInts[1] << "."
+                           << hashInts[2];
+
+                PutIRC(sWebIRCMsg.str());
+                delete[] hashBytes;
             }
         }
 
@@ -201,6 +293,144 @@ class CSASLAuthMod : public CModule {
         }
     }
 
+    void SetSASLNetworkCommand(const CString& sLine) {
+        CString sNetwork = sLine.Token(1);
+        if (!sNetwork.empty()) {
+            SetNV("network_name", sNetwork);
+            PutModule(t_s("We will configure the [" + sNetwork +
+                          "] network for SASL."));
+        } else {
+            PutModule(
+                t_s("Usage: SetSASLNetwork afternet, SetSASLNetwork libera, "
+                    "SetSASLNetwork <network>"));
+        }
+    }
+
+    void SetSASLCommand(const CString& sLine) {
+        CString sEnable = sLine.Token(1);
+
+        if (sEnable == "no") {
+            m_bSaslImpersonation = false;
+        } else if (sEnable == "yes") {
+            if (!sLine.Token(2).empty())
+                SetNV("sasl_mechanism", sLine.Token(2));
+            if (!sLine.Token(3).empty()) SetNV("sasl_username", sLine.Token(3));
+            if (!sLine.Token(4).empty()) SetNV("sasl_password", sLine.Token(4));
+
+            m_bSaslImpersonation = true;
+
+            PutModule(t_f("SASL mechanism has been set to [{1}]")(
+                GetNV("sasl_mechanism")));
+            PutModule(t_f("SASL username has been set to [{1}]")(
+                GetNV("sasl_username")));
+            PutModule(t_f("SASL password has been set to [{1}]")(
+                GetNV("sasl_password")));
+        } else if (sEnable == "reset") {
+            m_bSaslImpersonation = false;
+            SetNV("sasl_mechanism", "");
+            SetNV("sasl_username", "");
+            SetNV("sasl_password", "");
+        } else {
+            if (m_bSaslImpersonation) {
+                CString sMechanism = GetNV("sasl_mechanism");
+                CString sUsername = GetNV("sasl_username");
+                CString sPassword = GetNV("sasl_password");
+
+                if (sMechanism.empty()) {
+                    PutModule(t_s("SASL mechanism is currently not set"));
+                } else {
+                    PutModule(t_f("SASL mechanism is currently set to '{1}'")(
+                        sMechanism));
+                }
+                if (sUsername.empty()) {
+                    PutModule(t_s("SASL username is currently not set"));
+                } else {
+                    PutModule(t_f("SASL username is currently set to '{1}'")(
+                        sUsername));
+                }
+                if (sPassword.empty()) {
+                    PutModule(t_s("SASL password was not supplied"));
+                } else {
+                    PutModule(t_s("Password was supplied"));
+                }
+            } else {
+                PutModule(
+                    t_s("Usage: SetSASL yes EXTERNAL, SetSASL yes PLAIN "
+                        "zncuser zncpassword, SetSASL no, SetSASL reset"));
+            }
+        }
+    }
+
+    void SetWEBIRCCommand(const CString& sLine) {
+        CString sEnable = sLine.Token(1);
+
+        if (sEnable == "no") {
+            m_bWEBIRC = false;
+        } else if (sEnable == "yes") {
+            if (!sLine.Token(2).empty())
+                SetNV("webirc_gateway", sLine.Token(2));
+            if (!sLine.Token(3).empty())
+                SetNV("webirc_password", sLine.Token(3));
+            if (!sLine.Token(4).empty())
+                SetNV("webirc_host_suffix", sLine.Token(4));
+
+            m_bWEBIRC = true;
+
+            PutModule(t_f("WEBIRC gateway has been set to [{1}]")(
+                GetNV("webirc_gateway")));
+            PutModule(t_f("WEBIRC password has been set to [{1}]")(
+                GetNV("webirc_password")));
+            PutModule(t_f("WEBIRC hostname suffix has been set to [{1}]")(
+                GetNV("webirc_host_suffix")));
+        } else if (sEnable == "reset") {
+            m_bWEBIRC = false;
+            SetNV("webirc_gateway", "");
+            SetNV("webirc_password", "");
+            SetNV("webirc_host_suffix", "");
+        } else {
+            if (m_bWEBIRC) {
+                CString sGateway = GetNV("webirc_gateway");
+                CString sPassword = GetNV("webirc_password");
+                CString sHostSuffix = GetNV("webirc_host_suffix");
+
+                if (sGateway.empty()) {
+                    PutModule(t_s("WEBIRC gateway is currently not set"));
+                } else {
+                    PutModule(t_f("WEBIRC gateway is currently set to '{1}'")(
+                        sGateway));
+                }
+                if (sPassword.empty()) {
+                    PutModule(t_s("WEBIRC password was not supplied"));
+                } else {
+                    PutModule(t_s("WEBIRC password was supplied"));
+                }
+                if (sHostSuffix.empty()) {
+                    PutModule(
+                        t_s("WEBIRC hostname suffix is currently not set"));
+                } else {
+                    PutModule(
+                        t_f("WEBIRC hostname suffix is currently set to '{1}'")(
+                            sHostSuffix));
+                }
+            } else {
+                PutModule(
+                    t_s("Usage: SetWEBIRC yes zncgateway zncpassword "
+                        "Users.AfterNET.Org, SetWEBIRC no, SetWEBIRC reset"));
+            }
+        }
+    }
+
+    void SetUserSaltCommand(const CString& sLine) {
+        CString sSalt = sLine.Token(1);
+        if (!sSalt.empty()) {
+            SetNV("user_salt", sSalt);
+            PutModule(
+                t_s("We will use [" + sSalt + "] to salt the username hash."));
+        } else {
+            PutModule(t_s("Usage: SetUserSalt <salt>"));
+        }
+    }
+
     bool CreateUser() const { return GetNV("CreateUser").ToBool(); }
 
     CString CloneUser() const { return GetNV("CloneUser"); }
@@ -213,6 +443,9 @@ class CSASLAuthMod : public CModule {
     sasl_callback_t m_cbs[2];
     CString m_sMethod;
 
+    bool m_bWEBIRC;
+    bool m_bSaslImpersonation;
+
     static int getopt(void* context, const char* plugin_name,
                       const char* option, const char** result, unsigned* len) {
         if (CString(option).Equals("pwcheck_method")) {
@@ -221,6 +454,13 @@ class CSASLAuthMod : public CModule {
         }
 
         return SASL_CONTINUE;
+    }
+
+    static inline uint8* downsample(uint8 (&i)[16]) {
+        uint8* r = new uint8[3]{(uint8)(i[0] ^ i[1] ^ i[2] ^ i[3] ^ i[4]),
+                                (uint8)(i[5] ^ i[6] ^ i[7] ^ i[8] ^ i[9]),
+                                (uint8)(i[10] ^ i[11] ^ i[12] ^ i[13] ^ i[15])};
+        return r;
     }
 };
 
